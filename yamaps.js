3e4ffd392e7c29e0b3de4d2c9998cc006c1eb70f
vadcode <vadgtd@gmail.com>
2019-08-26 21:33:53 +0400

    traffic light

--- yamaps.js
+++ yamaps.js
@@ -1,4 +1,6 @@
-var myMap;
+var myMap; // var or let or not )
+
+
 
 // Дождёмся загрузки API и готовности DOM.
 ymaps.ready(init);
@@ -10,14 +12,24 @@ function init () {
         // При инициализации карты обязательно нужно указать
         // её центр и коэффициент масштабирования.
         center: [55.76, 37.64], // Москва
-        zoom: 10
-    }, {
-        searchControlProvider: 'yandex#search'
-    });
+            zoom: 10,
+            controls: []
+        });
 
-    document.getElementById('destroyButton').onclick = function () {
+    // Создадим элемент управления "Пробки".
+    var trafficControl = new ymaps.control.TrafficControl({ state: {
+            // Отображаются пробки "Сейчас".
+            providerKey: 'traffic#actual',
+            // Начинаем сразу показывать пробки на карте.
+            trafficShown: true
+        }});
+    // Добавим контрол на карту.
+    myMap.controls.add(trafficControl);
+    // Получим ссылку на провайдер пробок "Сейчас" и включим показ инфоточек.
+    trafficControl.getProvider('traffic#actual').state.set('infoLayerShown', true);    
+
+     document.getElementById('destroyButton').onclick = function () {
         // Для уничтожения используется метод destroy.
         myMap.destroy();
     };
-
 }
